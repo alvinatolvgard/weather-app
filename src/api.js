9b9fb82a1cf299fa070fb56f5162e6d071f6a0bc
alvina <alvinatolvgard@gmail.com>
2026-02-19 12:23:29 +0100

    Add API setup with getWeatherForecast function

---
+++ src/api.js
@@ -0,0 +1,13 @@
+async function getWeatherForecast(city) {
+    const url = `https://api.weatherapi.com/v1/forecast.json?key=${CONFIG.API_KEY}&q=${city}&days=7&lang=sv`
+
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error("Couldn't get weatherdata");
+    }
+
+    const data = await response.json();
+    return data;
+}
